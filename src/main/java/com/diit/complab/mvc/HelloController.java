de8b48935897e5e106eaf3815088b26609308d32
6-pz <6-pz@i.ua>
2014-02-25 23:23:49 +0200

    добавил пример формы на бутстрапе

--- src/main/java/com/diit/complab/mvc/HelloController.java
+++ src/main/java/com/diit/complab/mvc/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/")
@@ -13,4 +14,18 @@ public class HelloController {
 		model.addAttribute("message", "Hello world!");
 		return "hello";
 	}
+
+    @RequestMapping(value = "form.html", method = RequestMethod.GET)
+    public String printForm(ModelMap model) {
+        return "form";
+    }
+
+    @RequestMapping(value = "get.json", method = RequestMethod.GET)
+    public String getModel(ModelMap model) {
+        model.addAttribute("message", "Hello world!");
+        model.addAttribute("message2", "Hello world!");
+        model.addAttribute("message3", "Hello world!");
+        return "jsonView";
+    }
+
 }
